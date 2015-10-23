9bbe1adf80c6efeeafbc1c646a70829296401731
priort <priort@gmail.com>
2015-10-23 12:29:24 +0100

    tests and prod code for play round

--- card-game-war/src/card_game_war/game.clj
+++ card-game-war/src/card_game_war/game.clj
@@ -1,13 +1,50 @@
 (ns card-game-war.game)
 
-;; feel free to use these cards or use your own data structure
 (def suits [:spade :club :diamond :heart])
+(def suit-value-by-name {:spade 1
+                         :club 2
+                         :diamond 3
+                         :heart 4})
 (def ranks [2 3 4 5 6 7 8 9 10 :jack :queen :king :ace])
+(def rank-value-by-name {2 2
+                         3 3
+                         4 4
+                         5 5
+                         6 6
+                         7 7
+                         8 8
+                         9 9
+                         10 10
+                         :jack 11
+                         :queen 12
+                         :king 13
+                         :ace 14})
 (def cards
   (for [suit suits
         rank ranks]
     [suit rank]))
 
-(defn play-round [player1-card player2-card])
+(defn- play-round-higher-rank-wins [[_ rank-1 :as player1-card]
+                  [_ rank-2 :as player2-card]]
+  (cond
+    (> (get rank-value-by-name rank-2) (get rank-value-by-name rank-1))
+    [[] [player1-card player2-card]]
+    (< (get rank-value-by-name rank-2) (get rank-value-by-name rank-1))
+    [[player1-card player2-card] []]))
+
+(defn- play-round-higher-suit-wins [[suit-1 _ :as player1-card]
+                                    [suit-2 _ :as player2-card]]
+  (cond
+    (> (get suit-value-by-name suit-2) (get suit-value-by-name suit-1))
+    [[] [player1-card player2-card]]
+    (< (get suit-value-by-name suit-2) (get suit-value-by-name suit-1))
+    [[player1-card player2-card] []]))
+
+(defn play-round [player1-card player2-card]
+
+  (println [player1-card player2-card])
+  (if-let [result (play-round-higher-rank-wins player1-card player2-card)]
+    result (play-round-higher-suit-wins player1-card player2-card)))
 
 (defn play-game [player1-cards player2-cards])
+
--- card-game-war/test/card_game_war/game_test.clj
+++ card-game-war/test/card_game_war/game_test.clj
@@ -2,18 +2,65 @@
   (:require [clojure.test :refer :all]
             [card-game-war.game :refer :all]))
 
+(defn- higher-card-wins? [lower-rank-card higher-rank-card]
+  (and
+    (= [[] [lower-rank-card higher-rank-card]] (play-round lower-rank-card higher-rank-card))
+    (= [[higher-rank-card lower-rank-card] []] (play-round higher-rank-card lower-rank-card))))
 
-;; fill in  tests for your game
+(defn get-results-from-play-round-suits-combos-lower-to-higher-rank-combos [suits ranks]
+  (for [suit-1 suits
+        suit-2 suits
+        rank-1-index (range 0 (count ranks))
+        rank-2-index (range (inc rank-1-index) (count ranks))
+        :let [rank-1 (nth ranks rank-1-index)
+              rank-2 (nth ranks rank-2-index)]]
+    (higher-card-wins? [suit-1 rank-1] [suit-2 rank-2])))
+
+(defn get-results-from-play-round-equal-ranks-combos-lower-to-higher-suit-combos [suits ranks]
+  (for [rank ranks
+        suit-1-index (range 0 (count suits))
+        suit-2-index (range (inc suit-1-index) (count suits))
+        :let [suit-1 (nth suits suit-1-index)
+              suit-2 (nth suits suit-2-index)]]
+    (higher-card-wins? [suit-1 rank] [suit-2 rank])))
+
+
 (deftest test-play-round
-  (testing "the highest rank wins the cards in the round"
-    (is (= 0 1)))
-  (testing "queens are higher rank than jacks")
-  (testing "kings are higher rank than queens")
-  (testing "aces are higher rank than kings")
-  (testing "if the ranks are equal, clubs beat spades")
-  (testing "if the ranks are equal, diamonds beat clubs")
+  (testing "the highest numeric rank wins the cards in the round"
+    (is (= true
+           (reduce #(and %1 %2) true
+                   (get-results-from-play-round-suits-combos-lower-to-higher-rank-combos
+                     suits (vec (range 2 11)))))))
+
+  (testing "queens are higher rank than jacks"
+    (is (= true
+           (reduce #(and %1 %2) true
+                   (get-results-from-play-round-suits-combos-lower-to-higher-rank-combos
+                     suits [:jack :queen])))))
+
+  (testing "kings are higher rank than queens"
+    (is (= true
+           (reduce #(and %1 %2) true
+                   (get-results-from-play-round-suits-combos-lower-to-higher-rank-combos
+                     suits [:queen :king])))))
+
+  (testing "aces are higher rank than kings"
+    (is (= true
+           (reduce #(and %1 %2) true
+                   (get-results-from-play-round-suits-combos-lower-to-higher-rank-combos
+                     suits [:king :ace])))))
+  (testing "if the ranks are equal, clubs beat spades"
+    (is (= true
+           (reduce #(and %1 %2) true
+                   (get-results-from-play-round-equal-ranks-combos-lower-to-higher-suit-combos
+                     [:spade :club] ranks)))))
+
+  (testing "if the ranks are equal, diamonds beat clubs"
+    (is (= true
+           (reduce #(and %1 %2) true
+                   (get-results-from-play-round-equal-ranks-combos-lower-to-higher-suit-combos
+                     [:club :diamond] ranks)))))
   (testing "if the ranks are equal, hearts beat diamonds"))
 
 (deftest test-play-game
   (testing "the player loses when they run out of cards"))
-
